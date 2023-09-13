4852f211ec049b0e0174c1d8cb1c785b7bfe0af9
allgeo <62227321+allgeo@users.noreply.github.com>
2023-09-12 23:58:00 -0400

    working 2 column

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,16 @@
     function refreshPage() {
         location.reload();
     }
+
+    // Disable page scroll
+    onMount(() => {
+      document.body.style.overflow = 'hidden';
+    });
 </script>
 
-<div class="flex flex-col h-screen mt-44 dot">
+
+<div class="flex flex-col h-screen mt-36 ">
+    
     <div class="flex items-center justify-center flex-shrink-0">
         <button on:click={refreshPage} class="p-2 px-4 mb-4 text-white bg-blue-500 rounded">
             click for random issues
@@ -18,24 +25,24 @@
     </div>
     <div class="flex-grow overflow-y-auto">
         <div class="flex flex-col items-center p-4">
-            <div class="max-w-4xl rounded github-data">
-                <ul>
+            <div class="max-w-full rounded github-data">
+                <ul class="grid grid-cols-2 gap-4">
                     {#if all}
                         {#each all as repo}
                             {#if repo.node.issues.edges.length > 0}
-                            <li class="flex items-center p-4 m-2 bg-gray-200 rounded repo-data">
+                            <li class="flex items-center p-4 bg-gray-200 rounded repo-data max-w-[600px]">
                                     <img class="flex-shrink-0 object-cover w-16 h-16 rounded" src="{repo.node.owner.avatarUrl}" alt="avatar">
                                     <div class="flex-grow ml-4">
-                                      <div class="flex justify-between">
-                                          <strong>{repo.node.nameWithOwner}</strong>
-                                          <span>✨ {repo.node.stargazers.totalCount}</span>
-                                      </div>
-                                      <ul>
+                                        <div class="flex justify-between">
+                                            <strong class="text-sm break-words max-w-[600px]">{repo.node.nameWithOwner}</strong>
+                                            <span>✨ {repo.node.stargazers.totalCount}</span>
+                                        </div>
+                                        <ul>
                                             {#each repo.node.issues.edges as issue}
                                             <li class="text-xs break-words max-w-[600px]"> 👉 {issue.node.title} <a class="text-xs text-sky-600" href={issue.node.url}> 🔗</a></li>
                                             {/each}
-                                      </ul>
-                                </div>
+                                        </ul>
+                                    </div>
                             </li>
                             
                             {/if}
